c9680f2567e0ac99d9de5873e092cdee85ae54bf
wade-zore <wadezore.wez@gmail.com>
2023-10-11 07:42:33 +0200

    start of django front end

--- backend/management/urls.py
+++ backend/management/urls.py
@@ -1,7 +1,7 @@
-# from django.urls import path
-# from .views import EmployeeListView, CreateEmployee
-#
-# urlpatterns = [
-#     path('', EmployeeListView.as_view(), name='home'),
-#     path('add_employee/', CreateEmployee.as_view(), name='add_employee')
-# ]
+from django.urls import path
+from .views import EmployeeListView, CreateEmployee
+
+urlpatterns = [
+    path('', EmployeeListView.as_view(), name='home'),
+    path('add_employee/', CreateEmployee.as_view(), name='add_employee')
+]
--- backend/management/views.py
+++ backend/management/views.py
@@ -1,23 +1,23 @@
-# from django.forms import inlineformset_factory
-# from django.urls import reverse_lazy
-# from django.views.generic import ListView, FormView
-#
-# from api.models import Employee, Skill
-#
-#
-#
-#
-# class EmployeeListView(ListView):
-#     model = Employee
-#     template_name = "home.html"
-#
-#
-# # class CreateEmployee(FormView):
-# #
-# #     template_name = "add_employee.html"
-# #     form_class = EmployeeForm
-# #     success_url = reverse_lazy("home")
-# #
-# #     def form_valid(self, form):
-#
-#
+from django.forms import inlineformset_factory
+from django.urls import reverse_lazy
+from django.views.generic import ListView, FormView
+
+from api.models import Employee, Skill
+
+from .forms import EmployeeForm
+
+
+class EmployeeListView(ListView):
+    model = Employee
+    template_name = "home.html"
+
+
+class CreateEmployee(FormView):
+
+    template_name = "add_employee.html"
+    form_class = EmployeeForm
+    success_url = reverse_lazy("home")
+
+    # def form_valid(self, form):
+
+
